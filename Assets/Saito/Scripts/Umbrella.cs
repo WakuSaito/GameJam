92af67f2e2d49c643e15ae7180fb16618606d480
WakuSaito <h1202319029@hiratagakuen.onmicrosoft.com>
2024-08-09 15:31:17 +0900

    傘作成

---
+++ Assets/Saito/Scripts/Umbrella.cs
@@ -0,0 +1,32 @@
+
+public class Umbrella
+{
+    //状態フラグ
+    public UMBRELLA_STATE state = UMBRELLA_STATE.OPEN;
+
+
+    //状態取得
+    public UMBRELLA_STATE GetState()
+    {
+        return state;
+    }
+
+    //傘の開閉
+    public void ChangeOpen()
+    {
+        if (state == UMBRELLA_STATE.NOHAVING) return;//持っていないなら変化しない
+
+        //開閉切り替え
+        if (state == UMBRELLA_STATE.OPEN)
+            state = UMBRELLA_STATE.CLOSE;
+        else
+            state = UMBRELLA_STATE.OPEN;
+    }
+    //傘を失くす
+    public void Lost()
+    {
+        if (state == UMBRELLA_STATE.NOHAVING) return;//持っていないなら変化しない
+
+        state = UMBRELLA_STATE.NOHAVING;//未所持にする
+    }
+}
